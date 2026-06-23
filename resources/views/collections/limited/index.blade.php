ecd8b3a4e3bcf6e290817aa87d362545f132e795
christinreev <xoxosvntn@gmail.com>
2026-06-23 12:28:22 +0300

    padding fix

--- resources/views/collections/limited/index.blade.php
+++ resources/views/collections/limited/index.blade.php
@@ -18,20 +18,23 @@
     <div class="container mx-auto py-10">
 
         {{-- HERO --}}
-        <div class="mb-10">
+        <div class="mb-12">
 
-            <span class="uppercase tracking-[0.35em] text-xs text-gray-400">
-                Exclusive Drop
-            </span>
+            <div>
+                <span class="uppercase tracking-[0.25em] text-sm text-gray-500">
+                    New Arrivals
+                </span>
 
-            <h1 class="text-5xl font-light mt-4">
-                Лимитированная коллекция
-            </h1>
+                <h1 class="text-4xl lg:text-5xl font-light mt-3">
+                    Новинки
+                </h1>
+            </div>
+
+            <div class="flex justify-end mt-6">
+                <x-view-switch />
+            </div>
 
         </div>
-
-        {{-- VIEW SWITCH --}}
-        <x-view-switch />
 
         {{-- Товары --}}
         @include('catalog._products', ['products' => $products])
--- resources/views/collections/new/index.blade.php
+++ resources/views/collections/new/index.blade.php
@@ -18,25 +18,23 @@
     <div class="container mx-auto py-8">
 
         {{-- Hero --}}
-        {{-- Hero --}}
-      {{-- Hero --}}
-<div class="mb-12">
+        <div class="mb-12">
 
-    <div>
-        <span class="uppercase tracking-[0.25em] text-sm text-gray-500">
-            New Arrivals
-        </span>
+            <div>
+                <span class="uppercase tracking-[0.25em] text-sm text-gray-500">
+                    New Arrivals
+                </span>
 
-        <h1 class="text-4xl lg:text-5xl font-light mt-3">
-            Новинки
-        </h1>
-    </div>
+                <h1 class="text-4xl lg:text-5xl font-light mt-3">
+                    Новинки
+                </h1>
+            </div>
 
-    <div class="flex justify-end mt-6">
-        <x-view-switch />
-    </div>
+            <div class="flex justify-end mt-6">
+                <x-view-switch />
+            </div>
 
-</div>
+        </div>
 
         {{-- Товары --}}
         @include('catalog._products', ['products' => $products])
